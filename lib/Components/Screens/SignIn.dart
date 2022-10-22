3c2a65191e724cc8480ca5a74519d44de903d089
Tim-dev-hub <timurteresenko@gmail.com>
2022-10-22 20:29:18 +0300

    Add functionality to sign in

--- lib/Components/Screens/SignIn.dart
+++ lib/Components/Screens/SignIn.dart
@@ -4,73 +4,104 @@ import 'package:design_system_provider/desing_typography.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:adobe_spectrum/Components/button.dart';
+import 'package:nz_ua/nzsiteapi/nz_api.dart';
+import 'package:nz_ua/nzsiteapi/types.dart';
 
 class SignInScreen extends StatefulWidget {
-  const SignInScreen({Key? key}) : super(key: key);
+  const SignInScreen({
+    Key? key,
+    required this.state,
+    required this.api,
+  }) : super(key: key);
+  final NeedLoginState state;
+  final NzApi api;
   @override
   State<StatefulWidget> createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  String username = '';
+  String password = '';
+
+  bool pending = false;
+  bool? success;
+
   @override
   Widget build(BuildContext context) {
     var design = Desing.of(context);
-    return Container(
-      color: design.colors.gray.shade100,
-      padding: design.layout.spacing600.horizontal,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: design.layout.spacing500.bottom,
-            child: Text.rich(
-              TextSpan(
-                children: [
-                  design.typography.text(
-                    'Sign in',
-                    size: design.typography.fontSize300.value,
-                    semantic: TextSemantic.heading,
-                  ),
-                ],
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Padding(
-            padding: design.layout.spacing400.bottom,
-            child: AdobeTextField(
-              label: 'Username (required)',
-            ),
-          ),
-          Padding(
-            padding: design.layout.spacing75.bottom,
-            child: AdobeTextField(
-              label: 'Password (required)',
-              inputType: InputFieldType.password,
-            ),
-          ),
-          Padding(
-            padding: design.layout.spacing300.bottom,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Spacer(),
+        Padding(
+          padding: design.layout.spacing500.bottom,
+          child: Text.rich(
+            TextSpan(
               children: [
-                Text(
+                design.typography.text(
+                  'Sign in ($success)',
+                  size: design.typography.fontSize300.value,
+                  semantic: TextSemantic.heading,
+                ),
+              ],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: design.layout.spacing400.bottom,
+          child: AdobeTextField(
+            label: 'Username (required)',
+            onChange: (s) => username = s,
+          ),
+        ),
+        Padding(
+          padding: design.layout.spacing75.bottom,
+          child: AdobeTextField(
+            label: 'Password (required)',
+            onChange: (s) => password = s,
+            inputType: InputFieldType.password,
+          ),
+        ),
+        Padding(
+          padding: design.layout.spacing300.bottom,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                child: Text(
                   'Forgot password?',
                   style: TextStyle(
-                    fontFamily: 'Adobe Clean',
                     fontSize: design.typography.fontSize50.value,
                     color: design.colors.gray.shade700,
                   ),
                 ),
-              ],
-            ),
+                onTap: () => widget.api.forgotPassword(),
+              ),
+            ],
           ),
-          AdobeButton(
-            label: 'Login',
-          )
-        ],
-      ),
+        ),
+        //todo: load state after click
+        AdobeButton(
+          label: 'Login (pending = $pending)',
+          isPending: pending,
+          onClick: () async {
+            setState(() => pending = true);
+            var r = await widget.api.login(
+              username,
+              password,
+            );
+            setState(() {
+              pending = false;
+              success = r;
+            });
+          },
+        ),
+        const Spacer(),
+        //todo alerts support
+        if (widget.state.alerts.isNotEmpty) Text('${widget.state.alerts[0].text} (type = ${widget.state.alerts[0].type})'),
+      ],
     );
   }
 }
