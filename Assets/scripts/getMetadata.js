25659ccb6212d8889be2ac3aadf5842c022f6170
Tim-dev-hub <timurteresenko@gmail.com>
2022-10-24 19:16:05 +0300

    Fix metadata parsing if metadata don't contains homework

--- Assets/scripts/getMetadata.js
+++ Assets/scripts/getMetadata.js
@@ -8,7 +8,13 @@ result.me = {
 }
 var leftBlock = document.getElementsByClassName('rs-block');
 var homework = leftBlock[0];
-result.comingHomework = Array.from(homework.children[1].children).map((e) => {
+
+//todo check when homework be
+var hasHomework = leftBlock.length == 4;
+
+console.log(hasHomework);
+
+result.comingHomework = !hasHomework ? [] :  Array.from(homework.children[1].children).map((e) => {
     //TODO: support links and bold in homework.exercise
     return {
         date: e.children[0].children[0].innerText + '' + e.children[0].children[1].innerText,
@@ -20,17 +26,19 @@ result.comingHomework = Array.from(homework.children[1].children).map((e) => {
         })
     };
 })
-result.latestMarks = Array.from(leftBlock[1].children[1].children).map((e) => {
+result.latestMarks = Array.from(leftBlock[hasHomework ? 1 : 0].children[1].children).map((e) => {
     return {
         value: parseInt(e.children[1].innerText),
         lesson: e.children[0].innerText
     }
 })
-result.closestBirthdays = Array.from(leftBlock[2].children[1].children).map((e) => {
+result.closestBirthdays = Array.from(leftBlock[hasHomework ? 2 : 1].children[1].children).map((e) => {
+    var name = e.children[0].innerText;
+    var date = e.children[2].innerText.replace(/\n/g, '').trim();
     return {
-        date: e.children[2].innerText,
+        date: date,
         user: {
-            fullName: e.children[0].innerText,
+            fullName: name,
             profileUrl: e.children[0].href
         }
     }
